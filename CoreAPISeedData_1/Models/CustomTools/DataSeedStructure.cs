598222dc33e8e5fa71a95bc2ffc65fd5322db10d
cagri.yolyapar <cagri.yolyapar@gmail.com>
2025-05-10 14:14:57 +0300

    Add project files.

---
+++ CoreAPISeedData_1/Models/CustomTools/DataSeedStructure.cs
@@ -0,0 +1,20 @@
+﻿using CoreAPISeedData_1.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreAPISeedData_1.Models.CustomTools
+{
+    public static class DataSeedStructure
+    {
+        public static void KategoriEkle(ModelBuilder modelBuilder)
+        {
+            Category c = new Category
+            {
+                Id = 1,
+                CategoryName = "Tatlılar",
+                Description = "Test verisidir"
+            };
+
+            modelBuilder.Entity<Category>().HasData(c);
+        }
+    }
+}
